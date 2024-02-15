b890e600b83f14ee7437b5e60391798c6b2a638f
Leonardo Santos <leonardojosantos@gmail.com>
2024-02-15 18:53:42 -0300

    47. Task Template

--- 04_projeto_gerenciador_de_tarefas_htmx_bootsrap_express_sequelize/api/index.js
+++ 04_projeto_gerenciador_de_tarefas_htmx_bootsrap_express_sequelize/api/index.js
@@ -77,7 +77,36 @@ app.get('/todos', async (req, res) => {
         }
 
         // tarefas resgatadas
-        let html = tasks.map((task) => `<p>${task.text}</p>`).join('');
+        let html = tasks.map((task) => (
+            `<div class="card mb-3 ${task.complete ? "bg-light border-success" : ""}">
+                <div class="card-body ${task.complete ? "font-talic" : ""}">
+                    <h5 class="card-title">
+                        ${task.text}
+                    </h5>
+                    <p class="card-text">
+                        Dificuldade: ${task.difficulty}
+                    </p>
+                    <p class="card-text">
+                        Status: ${task.complete ? "Completa" : "Incompleta"}
+                    </p>
+                    <button
+                        class="btn btn-primary"
+                        onclick="editTask(${task.id}, '${task.text}', '${task.difficulty}')">
+                        Editar
+                    </button>
+                    <button
+                        class="btn btn-danger"
+                        onclick="deleteTask(${task.id})">
+                        Deletar
+                    </button>
+                    <button
+                        class="btn btn-secondary"
+                        onclick="toggleTask(${task.id})">
+                        ${task.complete ? "Desmarcar" : "Marcar como completa"}
+                    </button>
+                </div>
+            </div>`
+        )).join('');
 
         res.send(html);
     }
