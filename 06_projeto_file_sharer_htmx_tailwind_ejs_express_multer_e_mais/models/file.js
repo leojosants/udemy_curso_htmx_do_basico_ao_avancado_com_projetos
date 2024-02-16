f41931e369c18dbb925099f46cc0fb8aaa4eb44e
Leonardo Santos <leonardojosantos@gmail.com>
2024-02-16 16:04:04 -0300

    64. Creating models

---
+++ 06_projeto_file_sharer_htmx_tailwind_ejs_express_multer_e_mais/models/file.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class File extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      File.belongsTo(models.User,
+        {
+          foreignKey: 'userId',
+        }
+      );
+    }
+  }
+  
+  File.init(
+    {
+      nome: DataTypes.STRING,
+      descricao: DataTypes.TEXT,
+      caminho: DataTypes.STRING,
+      userId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        }
+      },
+    },
+    {
+      sequelize,
+      modelName: 'File',
+    }
+  );
+
+  return File;
+};
