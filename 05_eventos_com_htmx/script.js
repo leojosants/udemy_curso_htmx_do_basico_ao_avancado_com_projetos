969cd53c5fdc04cf877ca60a4a465fd4f2b257ec
Leonardo Santos <leonardojosantos@gmail.com>
2024-02-16 15:10:34 -0300

    60. Custom events

--- 05_eventos_com_htmx/script.js
+++ 05_eventos_com_htmx/script.js
@@ -1,42 +1,52 @@
 // aula 01
-// document.body.addEventListener('htmx:load', function (event) {
-//     // console.log(event);
-//     console.log('Carregou a página!');
-// });
+document.body.addEventListener('htmx:load', function (event) {
+    // console.log(event);
+    console.log('Carregou a página!');
+});
 
 // aula 02
-// document.body.addEventListener('htmx:beforeSwap', function (event) {
-//     console.log('Antes do Swap!');
-// });
-
-// document.body.addEventListener('htmx:afterSwap', function (event) {
-//     console.log('Depois do Swap!');
-// });
+// doc;
 
 // aula 03
-// document.body.addEventListener('htmx:beforeRequest', function (event) {
-//     console.log('Antes do Request!');
-// });
+document.body.addEventListener('htmx:beforeRequest', function (event) {
+    console.log('Antes do Request!');
+});
 
-// document.body.addEventListener('htmx:afterRequest', function (event) {
-//     console.log('Depois do Request!');
-// });
+document.body.addEventListener('htmx:afterRequest', function (event) {
+    console.log('Depois do Request!');
+});
 
 // aula 04
-// document.body.addEventListener('htmx:responseError', function (event) {
-//     alert('Ocorreu um erro interno, tente mais tarde!');
-// });
+document.body.addEventListener('htmx:responseError', function (event) {
+    alert('Ocorreu um erro interno, tente mais tarde!');
+});
 
 // aula 05
-// document.body.addEventListener('htmx:configRequest', function (event) {
-//     event.detail.headers['new_token'] = 'dkgorivn975h38';
-//     event.detail.parameters['param1'] = 'valor do param 1';
-//     event.detail.parameters['param2'] = 'valor do param 2';
-// });
+document.body.addEventListener('htmx:configRequest', function (event) {
+    event.detail.headers['new_token'] = 'dkgorivn975h38';
+    event.detail.parameters['param1'] = 'valor do param 1';
+    event.detail.parameters['param2'] = 'valor do param 2';
+});
 
 // aula 06
-// htmx.logger = function (event, element, data) {
-//     console.log(event);
-//     console.log(element);
-//     console.log(data);
-// }
+htmx.logger = function (event, element, data) {
+    console.log(event);
+    console.log(element);
+    console.log(data);
+}
+
+// aula 08
+const customEventButton = document.querySelector('#custom-event-button');
+
+customEventButton.addEventListener('click', function () {
+    htmx.trigger(customEventButton, 'customEvent');
+});
+
+document.body.addEventListener('customEvent', function (event) {
+    console.log('Evento acionado!');
+    htmx.ajax(
+        'GET',
+        'http://localhost:3000/customEvent',
+        '#custom-event-response'
+    );
+});
