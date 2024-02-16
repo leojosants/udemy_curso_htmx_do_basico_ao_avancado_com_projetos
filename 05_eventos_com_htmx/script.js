5d3f55c35d825dbba9d136fa2b741be5eac30feb
Leonardo Santos <leonardojosantos@gmail.com>
2024-02-16 13:57:55 -0300

    54. Events before and after content swap

--- 05_eventos_com_htmx/script.js
+++ 05_eventos_com_htmx/script.js
@@ -1,4 +1,12 @@
 document.body.addEventListener('htmx:load', function (event) {
-    console.log(event);
+    // console.log(event);
     console.log('Carregou a página!');
+});
+
+document.body.addEventListener('htmx:beforeSwap', function (event) {
+    console.log('Antes do Swap!');
+});
+
+document.body.addEventListener('htmx:afterSwap', function (event) {
+    console.log('Depois do Swap!');
 });
