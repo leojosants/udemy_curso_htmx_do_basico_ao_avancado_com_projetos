1d4acb807d16ef3bdfde6cc2624f26e27b871631
Leonardo Santos <leonardojosantos@gmail.com>
2024-02-17 01:27:14 -0300

    75. File Upload

--- 06_projeto_file_sharer_htmx_tailwind_ejs_express_multer_e_mais/routes/admin.js
+++ 06_projeto_file_sharer_htmx_tailwind_ejs_express_multer_e_mais/routes/admin.js
@@ -1,6 +1,8 @@
 
 const express = require('express');
+const upload = require('../config/multerConfig');
 const { User } = require('../models');
+const { File } = require('../models');
 const isAuthenticated = require('../middleware/isAuthenticated');
 const router = express.Router();
 
@@ -16,6 +18,29 @@ router.get('/all-files', isAuthenticated, (req, res) => {
         userId: req.session.userId,
         files: [],
     });
+});
+
+router.post('/upload', isAuthenticated, upload.single('arquivo'), async (req, res) => {
+    const { nome, descricao } = req.body;
+    const userId = req.session.userId;
+
+    if (!nome || !req.file) {
+        res.status(422).send('Preencha todos os campos!');
+        return;
+    }
+
+    const caminho = req.file.path;
+
+    try {
+        await File.create({ nome, descricao, caminho, userId });
+
+        // buscar e enviar lista de arquivos
+        res.send('Deu certo!');
+
+    }
+    catch (error) {
+        res.send('Erro ao criar arquivo!');
+    }
 });
 
 module.exports = router;
